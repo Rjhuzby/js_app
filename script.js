62c520467c5e51a89f5f285d25416a1cb19b0e7a
Artemiy <klarcky@yandex.ru>
2023-03-31 16:53:20 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// alert ('Hello!');
+
+// const input = confirm('Are you crazy?');
+// console.log(input);
+
+// const answer = +prompt('Vam est 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Artemiy';
+
+// alert(`Привет, ${user}!`)
+
+// let incr = 10;
+// let decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(++incr, decr--)
+// console.log(incr, decr)
+
+// console.log(2 === 2);
+
+    const isChecked = false,
+        isClose = false;
+
+    console.log(isChecked || !isClose);
